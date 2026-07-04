69690c54421b5ae0a34d66b273d0d04d43d97811
bipin_09 <bipinpala30@gmail.com>
2026-07-04 16:56:15 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user",(req,res)=>{
-    res.send("get the user dashboard");
-})
+// app.get("/user",(req,res)=>{
+//     res.send("get the user dashboard");
+// })
 
 app.post("/user",(req,res)=>{
     res.send("save the user data sucessfully");
@@ -17,9 +17,21 @@ app.patch("/user",(req,res)=>{
 app.delete("/user",(req,res)=>{
     res.send("user dashboard deleted sucessfully!");
 })
-app.use("/",(req,res)=>{
-    res.send("Namaste ji!!");
+// app.use("/",(req,res)=>{
+//     res.send("Namaste ji!!");
+// })
+
+//playing with the route things
+app.get("/user",
+    (req,res,next)=>{
+    console.log("fisrt route handler invoked!!");
+    next();
+},
+(req,res)=>{
+   console.log("second handler function invoked")
+   res.send("2nd responce!!");
 })
+
 
 app.listen(3000,()=>{
     console.log("the server is running on port 3000");
