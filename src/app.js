b7cc25c35a89a6a9186f3e16890b21f498c60053
bipin_09 <bipinpala30@gmail.com>
2026-07-25 23:51:37 +0530

    crud-APIs-done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,53 @@ const User = require("./models/user");
 
 app.use(express.json())
 
+//find user from the database
+// app.get("/user",async (req,res)=>{
+//     try{
+//        const user =  await User.find({email : req.body.email});
+//         res.send(user);
+//     }catch(err){
+//         res.status(400).send("somthing went wrong!!!");
+//     }
+    
+// })
+
+//bulding feed page of the website
+app.get("/feed",async (req,res)=>{
+    try{
+        const users = awaitUser.find({});
+        if(users.length === 0) res.status(400).send("user not found!!");
+        res.send(users);
+    }catch(err){
+        res.send("Somthing went wrong!!");
+    }
+})
+
+//delete from the data base by findig from the ID
+app.delete("/deleteuser",async (req,res)=>{
+    const userId = req.body.userId;
+    try{
+        await User.findByIdAndDelete(userId);
+        res.send("user deleted sucessfully!!");
+    }
+    catch(err){
+        res.status(400).send("somthing went wrong!!!");
+    }
+})
+
+
+//update the user by finding through id
+app.patch("/updateuser",async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        await User.findByIdAndUpdate(userId,req.body);
+        res.send("user updated sucessfully!!")
+    }
+    catch(err){
+        res.status(400).send("Somthing went wrong!!");
+    }
+})
+//sign up in the application
 app.post("/signup",async(req,res)=>{
         console.log(req.body);
         const user = new User(req.body);
@@ -18,7 +65,7 @@ app.post("/signup",async(req,res)=>{
         
 })
 
-
+//connect to the database and start the application
 connectDB()
     .then(()=>{
         console.log("Database connection established Sucessfully!!");
