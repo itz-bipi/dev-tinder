ffaa27955e8d2a5e3a4a880a63e0005918ad6241
bipin_09 <bipinpala30@gmail.com>
2026-08-01 14:08:15 +0530

    Api-validate-applied

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const app = express();
-
 const User = require("./models/user");
 
-app.use(express.json())
+
+const app = express();
+app.use(express.json());
 
 //find user from the database
 // app.get("/user",async (req,res)=>{
@@ -14,64 +14,84 @@ app.use(express.json())
 //     }catch(err){
 //         res.status(400).send("somthing went wrong!!!");
 //     }
-    
+
 // })
 
 //bulding feed page of the website
-app.get("/feed",async (req,res)=>{
-    try{
-        const users = awaitUser.find({});
-        if(users.length === 0) res.status(400).send("user not found!!");
-        res.send(users);
-    }catch(err){
-        res.send("Somthing went wrong!!");
-    }
-})
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) res.status(400).send("user not found!!");
+    res.send(users);
+  } catch (err) {
+    res.send("Somthing went wrong!!");
+  }
+});
 
 //delete from the data base by findig from the ID
-app.delete("/deleteuser",async (req,res)=>{
-    const userId = req.body.userId;
-    try{
-        await User.findByIdAndDelete(userId);
-        res.send("user deleted sucessfully!!");
-    }
-    catch(err){
-        res.status(400).send("somthing went wrong!!!");
-    }
-})
-
+app.delete("/deleteuser", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("user deleted sucessfully!!");
+  } catch (err) {
+    res.status(400).send("somthing went wrong!!!");
+  }
+});
 
 //update the user by finding through id
-app.patch("/updateuser",async(req,res)=>{
-    const userId = req.body.userId;
-    try{
-        await User.findByIdAndUpdate(userId,req.body,{runValidators : true});
-        res.send("user updated sucessfully!!")
-    }
-    catch(err){
-        res.status(400).send("Update failed!!" + err.message);
-    }
-})
+app.patch("/updateuser", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    //what feild a user can update after creating an acount
+    const ALLOWED_UPDATES = [
+    "userId",
+    "password",
+    "age",
+    "gender",
+    "photourl",
+    "about",
+    "skills",
+  ];
+  const isAllowled = Object.keys(req.body).every((k) => {
+    return ALLOWED_UPDATES.includes(k);
+  });
+  console.log(isAllowled);
+  if (!isAllowled) {
+   throw new Error("Update is not valid for this feilds!!!");
+  }
+console.log(req.body.skills.length);
+  if(req.body.skills.length > 10){
+    throw new Error("Skills should be less than 10!!")
+  }
+
+    await User.findByIdAndUpdate(userId, req.body, { runValidators: true });
+    res.send("user updated sucessfully!!");
+  } catch (err) {
+    res.status(400).send("Update failed!!" + err.message);
+  }
+});
 //sign up in the application
-app.post("/signup",async(req,res)=>{
-        console.log(req.body);
-        const user = new User(req.body);
-        try{
-            await user.save();
-            res.send("user sign up sucessfully!!");
-        }catch(err){
-            res.status(400).send("sign up failed!!");
-        }
-        
-})
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("user sign up sucessfully!!");
+  } catch (err) {
+    res.status(400).send("sign up failed!!" + err.message);
+  }
+});
 
 //connect to the database and start the application
 connectDB()
-    .then(()=>{
-        console.log("Database connection established Sucessfully!!");
-        app.listen(3000,()=>{
-            console.log("app is running in port 3000")
-        })
-    }).catch((err)=>{
-        console.log("Database cnnection failed!" + err);
-    })
+  .then(() => {
+    console.log("Database connection established Sucessfully!!");
+    app.listen(3000, () => {
+      console.log("app is running in port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Database cnnection failed!" + err);
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName : {
@@ -13,11 +14,19 @@ const userSchema = new mongoose.Schema({
         required : true,
         unique : true,
         lowercase : true,
-        trim : true
+        trim : true,
+        validate(value){
+            if(!validator.isEmail(value)) throw new Error("email is not valid!!");
+          
+        }
     },
     password : {
         type : String,
         required : true,
+        validate(value){
+            if(!validator.isStrongPassword(value)) throw new Error("Password is not strong enough!!!!");
+          
+        }
     },
     age : {
         type : Number,
@@ -35,7 +44,11 @@ const userSchema = new mongoose.Schema({
     },
     photourl :{
         type : String,
-        default :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh1pLlmnu9qHOG3fyLc3uTB7VpCbi5a6F0CxzgaEPBzQ&s=10"
+        default :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh1pLlmnu9qHOG3fyLc3uTB7VpCbi5a6F0CxzgaEPBzQ&s=10",
+        validate(value){
+            if(!validator.isURL(value)) throw new Error("Not a valid photo url!!");
+          
+        }
     },
     about:{
         type : String,
